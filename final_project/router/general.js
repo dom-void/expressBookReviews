5f3a0d03aa6b22923b543e0fb523b169e6ec1695
dom void <doe@onet.pl>
2025-06-03 13:05:18 +0200

    Solve Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,8 +27,15 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const { author } = req.params;
+  const booksByAuthor = Object.values(books).filter(
+    (book) => book.author === author
+  );
+  if (booksByAuthor.length) {
+    return res.send(JSON.stringify(booksByAuthor));
+  } else {
+    return res.status(404).json({ message: "Resource not found" });
+  }
 });
 
 // Get all books based on title
