5a058b62ef84ff4e84eb7db4b5cdfe991232632b
dom void <doe@onet.pl>
2025-06-03 12:36:53 +0200

    Solve Task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -16,8 +16,13 @@ public_users.get("/", function (req, res) {
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const { isbn } = req.params;
+  const selectedBook = books[isbn];
+  if (selectedBook) {
+    return res.send(JSON.stringify(selectedBook));
+  } else {
+    return res.status(404).json({ message: "Resource not found" });
+  }
 });
 
 // Get book details based on author
